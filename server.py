d8ae20e73f74772e6280dca78545b12780ea6e53
Mendez9896 <andresmendez9896@gmail.com>
2020-12-14 18:13:32 -0400

    Parte mendez

--- server.py
+++ server.py
@@ -11,24 +11,24 @@ broker = 'broker.hivemq.com'
 port = 1883
 publishTopic="rams/publish2"
 subscribeTopic="rams/subscribe"
-
+topicApp= "2587/App"
 msg_count = 1
 
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 
-# estructura = [{"1A": 1, "2A": 1},{},{},{},{}]
+estructura = [{"1A": 1, "2A": 1},{},{},{},{}]
 # piso = 1
 # estructura[piso-1]["1A"]
 
-# estructura2 = {"12345":[1,2],"654321": [2,4]}
+estructura2 = {}
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
             print("publishTopic:'"+publishTopic+"'")
-            print("subscribeTopic:'"+subscribeTopic+"'")
+            print("subscribeTopic:'"+topicApp+"'")
         else:
             print("Failed to connect, return code %d\n", rc)
 
@@ -44,6 +44,41 @@ def subscribe(client: mqtt_client):
         publish(client)
     client.subscribe(subscribeTopic)
     client.on_message = on_message
+
+def subscribeApp(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        payloadData = msg.payload.decode().split(",")
+        if payloadData[0] == "ocupar" :
+            nd = {payloadData[3]: [payloadData[1],payloadData[2]]}
+            estructura2.update(nd)
+        elif payloadData[0] == "buscar" :
+            keys = estructura2.keys()
+            if payloadData[1] in keys :
+                publishBuscarParqueo(client, estructura2.get(payloadData[1]))
+            else:
+                publishBuscarParqueo(client, 0)
+            
+
+
+
+    client.subscribe(topicApp)
+    client.on_message = on_message
+def publishBuscarParqueo(client, lugar):
+    time.sleep(1)
+    publishTopic = "2587/parqueo/buscar"
+    if lugar == 0 :
+        msg = 0
+    else:
+        msg = lugar[0] + "," + lugar[1]
+    
+    result = client.publish(publishTopic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{publishTopic}`")
+    else:
+        print(f"Failed to send message to topic {publishTopic}")
 
 
 def publish(client):    
@@ -63,7 +98,7 @@ def publish(client):
 def run():
     # print(estructura2["12345"][0])
     client = connect_mqtt()
-    subscribe(client)
+    subscribeApp(client)
     client.loop_forever()
 
 if __name__ == '__main__':
